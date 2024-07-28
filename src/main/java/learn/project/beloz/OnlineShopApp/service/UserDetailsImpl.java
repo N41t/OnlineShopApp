05002caeb8fd95dc94b1e6edac84b2e8cdeab6dc
Mikhail Belozerov <belozerov.mg@gmail.com>
2024-07-28 20:14:44 +0700

    Added UserDetailsImpl

---
+++ src/main/java/learn/project/beloz/OnlineShopApp/service/UserDetailsImpl.java
@@ -0,0 +1,23 @@
+package learn.project.beloz.OnlineShopApp.service;
+
+import learn.project.beloz.OnlineShopApp.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsImpl implements UserDetailsService {
+
+    private final UserRepository repository;
+
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
